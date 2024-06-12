7f254e2511d8fc264bc3c86d0ef9bdfad060c9c8
ArthurDelannoyazerty <ah.delannoy@gmail.com>
2024-06-12 20:30:29 +0200

    improved edge predictor

--- model/layers/sparse_attention_edge_predictor_layer.py
+++ model/layers/sparse_attention_edge_predictor_layer.py
@@ -11,17 +11,8 @@ class SparseAttentionEdgePredictorLayer(nn.Module):
 
 
     def forward(self, f, neighbors):
-        wq_f = self.wq.reshape(-1, 1) * f                   # Wq*f
-        wk_f = self.wk.reshape(-1, 1) * f                   # Wq*f
-        S = torch.exp(torch.matmul(wq_f.T, wk_f))           # e^((wq_f.T)*(wk_f))
-        
-        nonzero_neigh = neighbors.nonzero()                                                    # Find indexes of neighbors in graph
-        unique_first_elements, counts = torch.unique(nonzero_neigh[:, 0], return_counts=True)       # Count number of neighbors per node
-        split_tensors = list(torch.split(nonzero_neigh, tuple(counts)))                             # split indexes of neighbors into a list (1 element = 1 tensor of indexes)
-
-        temp = [[S[n[i,0], n[i,1]] for i in range(len(n))] for n in split_tensors]                  # For each node, get the S value for the neighbors indexes
-        summed = torch.Tensor([torch.sum(torch.Tensor(e)) for e in temp]).to("cuda" if torch.cuda.is_available() else "cpu")                           # Sum these results for each nodes
-        division = summed.unsqueeze(0).repeat(1, S.shape[1], 1)[0]                                  # Repeat the sum in S.shape[1] array => division per columns
-        final_term  = S / division
-
-        return final_term
+        wq_f = self.wq.reshape(-1, 1) * f               # Wq*f
+        wk_f = self.wk.reshape(-1, 1) * f               # Wq*f
+        S = torch.exp(torch.matmul(wq_f.T, wk_f))       # e^((wq_f.T)*(wk_f))
+        S = S / torch.matmul(neighbors.float(), S).T    # Division
+        return S
--- model/train.py
+++ model/train.py
@@ -13,57 +13,59 @@ from tqdm import tqdm
 torch.manual_seed(42)
 
 
+def train():
+    transformation = Transformation()
 
-transformation = Transformation()
-
-number_neigh_tri = 20
-stl_file_path = "3d_models/stl/Handle.stl"
-mesh_data = transformation.stl_to_mesh(stl_file_path)
-graph = transformation.mesh_to_graph(mesh_data)
-
-
-if len(graph._node)<20:
-    raise Exception("Input mesh does not have enough vertices. (More than 20 is needed)")
-
-graph_nodes = torch.Tensor(np.array(graph))
-graph_adjacency_matrix = torch.Tensor(nx.adjacency_matrix(graph).toarray())
-
-torch_dataset = MeshDataset("3d_models/stl/")
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-gnn_model = GNNSimplificationMesh(number_neigh_tri).to(device)
-optimizer = torch.optim.Adam(gnn_model.parameters(), lr=1e-5, weight_decay=0.99)
+    number_neigh_tri = 20
+    stl_file_path = "3d_models/stl/Handle.stl"
+    mesh_data = transformation.stl_to_mesh(stl_file_path)
+    graph = transformation.mesh_to_graph(mesh_data)
 
 
-for epoch in range(0, 1): 
-    print(f'Starting epoch {epoch+1}')
-    
-    current_loss = 0.0
-    for i, data in tqdm(enumerate(torch_dataset), total=len(torch_dataset), desc='Iterate data', leave=False):
+    if len(graph._node)<20:
+        raise Exception("Input mesh does not have enough vertices. (More than 20 is needed)")
+
+    graph_nodes = torch.Tensor(np.array(graph))
+    graph_adjacency_matrix = torch.Tensor(nx.adjacency_matrix(graph).toarray())
+
+    torch_dataset = MeshDataset("3d_models/stl/")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    gnn_model = GNNSimplificationMesh(number_neigh_tri).to(device)
+    optimizer = torch.optim.Adam(gnn_model.parameters(), lr=1e-5, weight_decay=0.99)
+
+
+    for epoch in range(0, 1): 
+        print(f'Starting epoch {epoch+1}')
         
-        start = time.time()
-        graph_nodes, graph_adjacency_matrix = torch_dataset[0]
-        graph_nodes, graph_adjacency_matrix = graph_nodes.to(device), graph_adjacency_matrix.to(device)
-        optimizer.zero_grad()
-        end = time.time()
-        print('init input : ', end - start)
-        selected_triangles = gnn_model(200, graph_nodes, graph_adjacency_matrix)
-        
-        start = time.time()
-        loss = total_loss(gnn_model.inclusion_score, graph_nodes, gnn_model.extended_graph_nodes, gnn_model.final_scores, selected_triangles, gnn_model.selected_triangles_indexes, graph)
-        end = time.time()
-        print('loss : ', end - start)
-        
-        start = time.time()
-        loss.backward()        
-        end = time.time()
-        print('backward : ', end - start)
-        optimizer.step()
-        
-        current_loss += loss.item()
-        if i % 500 == 499:
-            print('Loss after mini-batch %5d: %.3f' %
-                (i + 1, current_loss / 500))
-            current_loss = 0.0
+        current_loss = 0.0
+        for i, data in tqdm(enumerate(torch_dataset), total=len(torch_dataset), desc='Iterate data', leave=False):
             
-print('Training process has finished.')
+            start = time.time()
+            graph_nodes, graph_adjacency_matrix = torch_dataset[0]
+            graph_nodes, graph_adjacency_matrix = graph_nodes.to(device), graph_adjacency_matrix.to(device)
+            optimizer.zero_grad()
+            end = time.time()
+            print('init input : ', end - start)
+            selected_triangles = gnn_model(200, graph_nodes, graph_adjacency_matrix)
+            
+            start = time.time()
+            loss = total_loss(gnn_model.inclusion_score, graph_nodes, gnn_model.extended_graph_nodes, gnn_model.final_scores, selected_triangles, gnn_model.selected_triangles_indexes, graph)
+            end = time.time()
+            print('loss : ', end - start)
+            
+            start = time.time()
+            loss.backward()        
+            end = time.time()
+            print('backward : ', end - start)
+            optimizer.step()
+            
+            current_loss += loss.item()
+            if i % 500 == 499:
+                print('Loss after mini-batch %5d: %.3f' %
+                    (i + 1, current_loss / 500))
+                current_loss = 0.0
+                
+    print('Training process has finished.')
+
+train()
